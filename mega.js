b3897a388a00224af78dc2fb91de9487fea6d413
Chamijd <chamindumax28@gmail.com>
2025-05-16 06:22:58 +0530

    Update mega.js

--- mega.js
+++ mega.js
@@ -2,8 +2,8 @@ import * as mega from 'megajs';
 
 // Mega authentication credentials
 const auth = {
-    email: 'bot@gmail.com', // Replace with your Mega email
-    password: 'Malaka200', // Replace with your Mega password
+    email: 'pemase2145@neuraxo.com', // Replace with your Mega email
+    password: 'Chamindu2008', // Replace with your Mega password
     userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
